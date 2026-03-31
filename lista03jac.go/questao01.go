1b1e8f857b4380305be52366e427418caedd9225
Haniel-png <hanielsilva132@gmail.com>
2026-03-30 21:36:40 -0300

    Alterações

--- lista03jac.go/questao01.go
+++ lista03jac.go/questao01.go
@@ -5,6 +5,7 @@ func main() {
   var (
 	nota [numNotas]float64 
 	soma float64 = 0
+	media float64 
 	  )
 	fmt.Print("Informe a nota 1: ")
 	fmt.Scan(&nota[0]) 
@@ -12,7 +13,16 @@ func main() {
 	fmt.Scan(&nota[1]) 
 	fmt.Print("Informe a nota 3: ")
 	fmt.Scan(&nota[2])
+	
 	soma = nota[0] + nota[1] + nota[2]
-
-	fmt.Printf("A média é: %f\n ", soma/float64(numNotas))
+    
+	media = soma / float64(numNotas)
+	 
+	fmt.Printf("A média é: %f\n ", media)
+ 
+	for i := 0; i < numNotas; i++ {
+	if nota[i] > media {
+		fmt.Printf("Nota %d é maior que a média: %f\n", i, media)
+	}	
+ }
 }
--- lista03jac.go/questao03.go
+++ lista03jac.go/questao03.go
@@ -1,7 +1,7 @@
 package main
 import "fmt"
 func main() {
-  const numNotas int = 10 
+  const numNotas int = 10
   var (
 	nota [numNotas]float64 
   )
